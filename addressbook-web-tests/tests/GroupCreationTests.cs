c0e4a6ed86cc7979d2b099b6e8f3f735e1950b5e
boolean178@gmail.com <boolean178@gmail.com>
2021-07-30 18:27:03 +0300

    appmanager

---
+++ addressbook-web-tests/tests/GroupCreationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    { 
+        [Test]
+        public void GroupCreationTest()
+        {
+            app.Navigator.OpenHomePage();
+            app.Auth.Login(new AccountData("admin","secret"));
+            app.Navigator.GoToGroupsPage();
+            app.Groups.InitGroupCreation();
+            GroupData group = new GroupData("aaa");
+            group.Header = "ada";
+            group.Footer = "fdfs";
+            app.Groups.FillGroupForm(group);
+            app.Groups.SubmitGroupCreation();
+            app.Groups.ReturnToGroupPage();
+            app.Auth.Logout();
+        }
+    }
+}
+
